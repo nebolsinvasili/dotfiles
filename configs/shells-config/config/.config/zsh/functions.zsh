181013f5d218dc8a4730a548893037e1878ee6ca
Nebolsin Vasili <nebolsinvasili@gmail.com>
2026-01-16 04:35:58 +0300

    Fix stow linker.

--- configs/shells-config/config/.config/zsh/functions.zsh
+++ configs/shells-config/config/.config/zsh/functions.zsh
@@ -11,20 +11,6 @@
 # Posted by Simba, modified by community. See post 'Timeline' for change history␍
 # Retrieved 2026-01-06, License - CC BY-SA 4.0␍
 function ranger {
-#   local IFS=$'\t\n'
-#   local tempfile="$(mktemp -t tmp.XXXXXX)"
-#   local ranger_cmd=(
-#     command
-#     ranger
-#     --cmd="map Q chain shell echo %d > \"$tempfile\"; quitall"
-#   )
-#
-#   ${ranger_cmd[@]} "$@"
-#   if [[ -f "$tempfile" ]] && [[ "$(cat -- "$tempfile")" != "$PWD" ]]; then
-#     cd -- "$(cat -- "$tempfile")" || return
-#   fi
-#   command rm -f -- "$tempfile" 2>/dev/null
-# }
     local IFS=$'"'\t\n'"'
     local tempfile="$(mktemp -t tmp.XXXXXX)"
     local ranger_cmd=(
